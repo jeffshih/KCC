9e0cd271e8a13c824725560afe221995431fa2b0
jeffshih <jeffhfs1224@gmail.com>
2018-10-17 07:26:28 +0000

    change demo and fix README

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-# Color classifier
+# Color classifie
 
 ### Dependency 
 
-cuda > 9.0 
-keras = 2.2.4
-tensorflow = 1.11.0
-opencv = 3.4.3
+cuda > 9.0  
+
+keras = 2.2.4  
+
+tensorflow = 1.11.0  
+
+opencv = 3.4.3  
 
 
 ### Install procedure
 
-sudo pip install tensorflow-gpu
-sudo pip install keras
-sudo pip install opencv-python
+sudo pip install tensorflow-gpu   
+
+sudo pip install keras  
+
+sudo pip install opencv-python  
 
 
+### Usage
 
+python colorDemo.py -i <imagePath> 
+
+or
+
+download test dataset and run
+
+python colorDemo.py
--- colorDemo.py
+++ colorDemo.py
@@ -2,16 +2,35 @@ import cv2
 from colorClassifier import colorClassifier 
 import random
 import glob 
-
-modelPath = './colorModel1017.h5'
-
-classifier = colorClassifier.colorClassifier(modelPath)
-allimg = glob.glob('../TestSet_01_perColor_500/*/*.jpg')
-label = [i.split("/")[2] for i in allimg]
+import argparse
+from argparse import ArgumentParser
 
 
-for x in range(len(label)):
-    if random.random()>0.8:
-        inp = cv2.imread(allimg[x])
+
+
+
+if __name__ == "__main__":
+
+
+    modelPath = './colorModel1017.h5'
+
+    classifier = colorClassifier.colorClassifier(modelPath)
+
+    parser = ArgumentParser()
+    parser.add_argument("-i",'--image',help="input image path",dest="img")
+
+    args = parser.parse_args()
+
+    if args.img == None:
+        allimg = glob.glob('../TestSet_01_perColor_500/*/*.jpg')
+        label = [i.split("/")[2] for i in allimg]
+        for x in range(len(label)):
+            if random.random()>0.8:
+                inp = cv2.imread(allimg[x])
+                res = classifier.getColor(inp)
+                print "ground Truth is {} and result is {}".format(label[x],res)
+
+    else:
+        inp = cv2.imread(args.img)
         res = classifier.getColor(inp)
-        print "ground Truth is {} and result is {}".format(label[x],res)
+        print "result is {}".format(res)
